1d98ee5909f608d1685daa4c4968900dda018f3a
Grenby <Grenby@users.noreply.github.com>
2019-11-14 22:31:26 +0300

    HI

--- game/core/core.py
+++ game/core/core.py
@@ -1,7 +1,39 @@
+import math
+
+import pygame
+from pygame.sprite import Sprite, DirtySprite
+import resources
+
+from game.world.world import World
+
+
 class Core:
 
     def __init__(self):
-        pass
+        pygame.init()
+        self.sprite = DirtySprite()
+        self.image = pygame.image.load('resources\circle.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = (500, 500)
+        self.world = World()
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-    def start(self, data=None):
-        pass
+    def start(self):
+        delta = 1 / 60
+        done = True
+        x = 0.1
+        while done:
+            x += 0.1
+            x %= 255
+            for event in pygame.event.get():  # User did something
+                if (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE) or event.type == pygame.QUIT:
+                    done = False
+
+            y=round(120*(math.sin(x)+1))
+            self.screen.fill((25, y, 255-y))
+            self.screen.blit(self.image, self.rect)
+            self.world.step(delta)
+            self.clock.tick(int(1000 * delta))
+            pygame.display.flip()
+        pygame.quit()
