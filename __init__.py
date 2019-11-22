2d7da2dcabfe75d50f85abb7da746ebb90ec7120
Grenby <Grenby@users.noreply.github.com>
2019-11-23 00:36:58 +0300

    try catch

--- __init__.py
+++ __init__.py
@@ -4,7 +4,19 @@ from game.core.core import Core
 from game.ui_manager.screens import Screen_Game, Screen_Menu
 
 if __name__ == '__main__':
-    output_file = open('resources/settings.json', 'r')
-    d = json.loads(output_file.read())
-    output_file.close()
-    Core(d).start(Screen_Menu())
+    d = None
+    try:
+        output_file = open('resources/settings.json', 'r')
+        d = json.loads(output_file.read())
+        output_file.close()
+    except FileNotFoundError as e:
+        d = {"width": 1920, "height": 1080, "fps": True}
+
+        j = json.dumps(d)
+
+        f = open("resources/settings.json", "w")
+        f.write(j)
+        f.close()
+
+    finally:
+        Core(d).start(Screen_Menu())
